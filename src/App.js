2f2fd053343b23cdda178acef9522e8a82c106a9
Ekwebenem Chukwuebuka Collins <collinschudy@gmail.com>
2022-11-08 12:15:21 +0100

    rendered the homepage component in the App function

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import React from 'react';
 import './App.css'
 
-import HomePage from './components/homepage.component'
+import HomePage from './pages/homepage/homepage.component'
 
 
 function App(){

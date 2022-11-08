3c06458a4ec6ac81b07002fbd68ab70a3f4ec66c
Ekwebenem Chukwuebuka Collins <collinschudy@gmail.com>
2022-11-08 12:19:47 +0100

    broke down the homepage component into smaller ones of which menu-item file is one of

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import './menu-item.styles.css'
+
+
+const MenuItem = ({ title, imageUrl, size }) => (
+    <div 
+     className={`menu-item ${size}`}>
+        <div className="background-image" 
+        style={{
+            backgroundImage: `url(${imageUrl})`
+        }} />
+        <div className='content'>
+            <h2 className='title'>{title.toUpperCase()}</h2>
+            <span className='subtitle'>SHOP NOW</span>
+        </div>
+    </div>
+);
+
+export default MenuItem;
+
